1cf53cb7bde71207231cc00ab42aa8637f375fd4
Zaman Shajib <mbzshajib@gmail.com>
2015-12-04 00:33:06 +0600

    [Initial Structure] - business package and dummy class added.

---
+++ src/main/java/com/synd/application/oes/coreapp/biz/repository/BaseRepository.java
@@ -0,0 +1,14 @@
+package com.synd.application.oes.coreapp.biz.repository;
+ /**
+ * *****************************************************************
+ * @copyright  2015.
+ * @author - Md. Badi-Uz-Zaman Shajib
+ * @email  - mbzshajib@gmail.com
+ * @gitHub - https://github.com/mbzshajib 
+ * @date: 12/4/2015
+ * @time: 12:24 AM
+ * ****************************************************************
+ */    
+
+public abstract class BaseRepository {
+}
---
+++ src/main/java/com/synd/application/oes/coreapp/biz/service/BaseService.java
@@ -0,0 +1,16 @@
+package com.synd.application.oes.coreapp.biz.service;
+
+/**
+ * *****************************************************************
+ *
+ * @author - Md. Badi-Uz-Zaman Shajib
+ * @copyright 2015.
+ * @email - mbzshajib@gmail.com
+ * @gitHub - https://github.com/mbzshajib
+ * @date: 12/4/2015
+ * @time: 12:23 AM
+ * ****************************************************************
+ */
+
+public abstract class BaseService {
+}
